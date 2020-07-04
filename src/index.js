5910b1eddb6fd04818b355482c6a1c9656adaa75
Gustavo Morales <gus.morales@me.com>
2020-07-04 02:03:51 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const history = createBrowserHistory()
 ReactDOM.render(
-  <React.StrictMode>
+  <Router history={history}>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
